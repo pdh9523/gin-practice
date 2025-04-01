e24fa74e17bd6add8c3acbc8c732b03eac219b2b
박동현 <jinee9523@gmail.com>
2025-04-02 00:19:04 +0900

    Fix: DB마운트 위치 변경

--- internal/routes/router.go
+++ internal/routes/router.go
@@ -4,20 +4,11 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/pdh9523/gin-practice/internal/domain/auth"
 	"github.com/pdh9523/gin-practice/internal/domain/post"
-	postModel "github.com/pdh9523/gin-practice/internal/domain/post/model"
 	"github.com/pdh9523/gin-practice/internal/domain/user"
-	userModel "github.com/pdh9523/gin-practice/internal/domain/user/model"
 	"github.com/pdh9523/gin-practice/internal/infra/cache"
-	"github.com/pdh9523/gin-practice/internal/infra/db"
-	"time"
 )
 
-func SetupRouter() *gin.Engine {
-	// DB 마운트
-	db.Init()
-	db.DB.AutoMigrate(&userModel.User{}, &postModel.Post{})
-
-	cacheStore := cache.NewGoCacheStore(10*time.Minute, 30*time.Minute)
+func SetupRouter(cacheStore cache.GlobalCacheStore) *gin.Engine {
 
 	r := gin.Default()
 	r.Group("/api/v1/")
--- main.go
+++ main.go
@@ -2,7 +2,13 @@ package main
 
 import (
 	"github.com/joho/godotenv"
+	postModel "github.com/pdh9523/gin-practice/internal/domain/post/model"
+	userModel "github.com/pdh9523/gin-practice/internal/domain/user/model"
+	"github.com/pdh9523/gin-practice/internal/infra/cache"
+	"github.com/pdh9523/gin-practice/internal/infra/db"
+	"github.com/pdh9523/gin-practice/internal/infra/redis"
 	"github.com/pdh9523/gin-practice/internal/routes"
+	"time"
 )
 
 func main() {
@@ -12,7 +18,17 @@ func main() {
 		panic(".env file not found")
 	}
 
+	// DB 마운트
+	db.Init()
+	db.DB.AutoMigrate(&userModel.User{}, &postModel.Post{})
+
+	cacheStore := cache.NewGoCacheStore(10*time.Minute, 30*time.Minute)
+
+	redis.InitRedis()
+	// 컨슈머 등록
+	redis.StartEmailVerifiedConsumer()
+
 	// 라우터 마운트
-	r := routes.SetupRouter()
+	r := routes.SetupRouter(cacheStore)
 	r.Run(":8080")
 }
